b675a1c367959bf453e915019a84cd911424b42f
talhaakhann1 <talhakhan3620@gmail.com>
2026-08-10 23:55:01 +0500

    feat: define route for order

---
+++ server/src/routes/order.route.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { verifyJWT, verifyRoles } from "../middlewares/auth.middleware.js";
+import { createOrder, getAllOrders, getOrderById } from "../controllers/order.controller.js";
+
+const router=Router();
+
+router.route('/:courseId/create').post(verifyJWT,createOrder)
+router.route('/all').get(verifyJWT,verifyRoles(["admin"]),getAllOrders)
+router.route('/:orderId').get(verifyJWT,getOrderById)
+
+export default router
