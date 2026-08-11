bae9ee957db3f7d46e6107c29091fcd036e5de41
talhaakhann1 <talhakhan3620@gmail.com>
2026-08-11 23:54:13 +0500

    feat: create landing page layout for lms

---
+++ client/src/app/(home)/page.tsx
@@ -0,0 +1,58 @@
+"use client"
+import Hero from "../../components/landing/Hero";
+import Footer from "../../components/layout/Footer";
+import { FeaturedCourses } from "../../components/landing/FeaturedCourses";
+import { Header } from "../../components/layout/Header";
+import TestimonialsPage from "../../components/pages/TestimonialPage";
+import FaqsPage from "../../components/pages/FaqsPage";
+import InstructorsPage from "../../components/pages/InstructorPage";
+import { courseService } from "@/src/services/course.service";
+import { useEffect, useState } from "react";
+import { Course } from "@/src/types/interfaces/course.interface";
+import { useAppSelector } from "@/src/store/hook";
+import { useRouter } from "next/navigation";
+import { showError } from "@/src/components/ui/toaster";
+import { AxiosError } from "axios";
+import ApiResponse from "@/src/utils/ApiResponse";
+
+export default function Home() {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+      const [courses, setCourses] = useState<Course[]>([])
+      const user=useAppSelector((state)=>state.auth.user)
+      const userRole=user?.role
+      const router=useRouter()
+      const fetchAllCourses = async () => {
+          setIsLoading(true)
+          try {
+              const res = await courseService.getAll()
+               console.log("API response:", res);
+              // console.log(res);
+             setCourses(res);
+              // toast
+          } catch (error) {
+            const axiosError = error as AxiosError<ApiResponse<unknown>>;
+            
+                const errorMessage =
+                  axiosError.response?.data.message ?? "Something went wrong";
+            
+                console.error(errorMessage);
+            
+                showError("Something went wrong", errorMessage);
+          } finally {
+              setIsLoading(false)
+          }
+      }
+      useEffect(() => {
+          fetchAllCourses()
+      }, [])
+  return (
+    <>
+      <Hero />
+      <FeaturedCourses courses={courses} isLoading={isLoading} role={userRole} />
+      <InstructorsPage />
+      <TestimonialsPage />
+      <FaqsPage />
+      <Footer />
+    </>
+  )
+}
