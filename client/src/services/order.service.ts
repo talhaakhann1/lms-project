870118c99715098fa7fc0feedff7f57a159b382e
talhaakhann1 <talhakhan3620@gmail.com>
2026-08-17 19:20:22 +0500

    feat: add order api service

---
+++ client/src/services/order.service.ts
@@ -0,0 +1,29 @@
+import { api } from "../libs/axios";
+import ApiResponse from "../utils/ApiResponse";
+import { Order } from "../types/interfaces/order.interface";
+
+class OrderService {
+  async create(courseId: string): Promise<Order> {
+    const response = await api.post<ApiResponse<Order>>(
+      `/api/orders/${courseId}/create`
+    );
+
+    return response.data.data;
+  }
+
+  async getAll(): Promise<Order[]> {
+    const response = await api.get<ApiResponse<Order[]>>(
+      "/api/orders/all"
+    );
+
+    return response.data.data;
+  }
+  async getById(orderId:string): Promise<Order> {
+    const response = await api.get<ApiResponse<Order>>(
+      `/api/orders/${orderId}`
+    );
+    return response.data.data;
+  }
+}
+
+export const orderService = new OrderService();
