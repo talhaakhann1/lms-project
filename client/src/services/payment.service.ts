d94bcbb55ca3a29434227cc40fa98151154fe127
talhaakhann1 <talhakhan3620@gmail.com>
2026-08-17 19:20:37 +0500

    feat: add payment api service

---
+++ client/src/services/payment.service.ts
@@ -0,0 +1,31 @@
+import { api } from "../libs/axios";
+import ApiResponse from "../utils/ApiResponse";
+import { Payment } from "../types/interfaces/payment.interface";
+
+class PaymentService {
+  async createPaymentSession(orderId: string): Promise<string> {
+  const response = await api.post<
+    ApiResponse<{ url: string }>
+  >(`/api/payments/create-payment-session/${orderId}`);
+
+  return response.data.data.url;
+}
+
+  async getByOrderId(orderId: string): Promise<Payment> {
+    const response = await api.get<ApiResponse<Payment>>(
+      `/api/payments/${orderId}`
+    );
+      return response.data.data;
+  }
+
+
+  async getByAll(): Promise<Payment[]> {
+    const response = await api.get<ApiResponse<Payment[]>>(
+      "/api/payments/all"
+    );
+  return response.data.data;
+  }
+
+}
+
+export const paymentService = new PaymentService();
