da3cf7c82fd56ea51b438e1bbacaeceebf360bf5
talhaakhann1 <talhakhan3620@gmail.com>
2026-08-20 15:41:22 +0500

    fix: updateProfile schema optional

--- server/src/Schemas/user.schema.ts
+++ server/src/Schemas/user.schema.ts
@@ -21,21 +21,31 @@ export const signInSchema=z.object({
     email:z.string(),
     password:z.string(),
 })
+const optionalString = (schema: z.ZodString) =>
+  schema.optional().or(z.literal(""));
 
 export const updateUserProfileSchema = z.object({
-   fullName:z.string()
+  fullName: optionalString(
+    z
+      .string()
       .trim()
       .min(2, "Name must be at least 2 characters")
-      .max(10, "Name must not exceed 10 characters").optional(),
-  title: z
-    .string()
-    .trim()
-    .min(2, "Title must be at least 2 characters")
-    .max(100, "Title must not exceed 100 characters").optional(),
+      .max(10, "Name must not exceed 10 characters")
+  ),
 
-  bio: z
-    .string()
-    .trim()
-    .min(10, "Bio must be at least 10 characters")
-    .max(1000, "Bio must not exceed 1000 characters").optional(),
+  title: optionalString(
+    z
+      .string()
+      .trim()
+      .min(2, "Title must be at least 2 characters")
+      .max(100, "Title must not exceed 100 characters")
+  ),
+
+  bio: optionalString(
+    z
+      .string()
+      .trim()
+      .min(10, "Bio must be at least 10 characters")
+      .max(1000, "Bio must not exceed 1000 characters")
+  ),
 });
